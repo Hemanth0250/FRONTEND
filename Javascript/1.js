3e9f28d327566b3a2488b25e6d9b9c3442be7379
Hemanth0250 <anuhemu25@gmail.com>
2025-07-07 12:09:47 +0530

    added Javascript and Typescript files

---
+++ Javascript/1.js
@@ -0,0 +1,25 @@
+
+// var x = 10;     // function scoped
+// let y = 20;     // block scoped
+// const z = 30;   // block scoped, can't be reassigned
+
+// console.log(x, y, z);
+
+// console.log(a); 
+// var a = 5;
+
+// console.log(b); 
+// let b = 10;
+let str = "hello";   // String
+let num = 42;        // Number
+let bool = true;     // Boolean
+let nothing = null;  // null
+let notDefined;      // undefined
+let big = 12345678901234567890n; // BigInt
+let sym = Symbol("c","hemu"); // Symbol
+
+console.log(big)
+console.log(sym)
+console.log(notDefined)
+console.log(nothing)
+console.log(bool)
