3f46308ab296bb698abba12456df8638c8bb5ce8
Hemanth0250 <anuhemu25@gmail.com>
2025-07-07 12:49:21 +0530

    practiced javascript till loops

--- Javascript/1.js
+++ Javascript/1.js
@@ -10,16 +10,73 @@
 
 // console.log(b); 
 // let b = 10;
-let str = "hello";   // String
-let num = 42;        // Number
-let bool = true;     // Boolean
-let nothing = null;  // null
-let notDefined;      // undefined
-let big = 12345678901234567890n; // BigInt
-let sym = Symbol("c","hemu"); // Symbol
+// let str = "hello";   // String
+// let num = 42;        // Number
+// let bool = true;     // Boolean
+// let nothing = null;  // null
+// let notDefined;      // undefined
+// let big = 12345678901234567890n; // BigInt
+// let sym = Symbol("c","hemu"); // Symbol
 
-console.log(big)
-console.log(sym)
-console.log(notDefined)
-console.log(nothing)
-console.log(bool)
+// console.log(big)
+// console.log(sym)
+// console.log(notDefined)
+// console.log(nothing)
+// console.log(bool)
+
+// let obj = { name: ["hemu","gowda"] , age: 22, email: "anuhemu@gmail.com" };
+
+// console.log(obj.name[0].slice(0,2))
+
+// let a = 5, b = 2;
+
+// console.log(a + b);   // Arithmetic
+// a += 3;               // Assignment
+// console.log(a === b); // Comparison
+// console.log(a > 1 && b < 3); // Logical
+// console.log(typeof a);       // Type
+
+// let c= 5
+// let d = "5"
+// console.log(c,"==",d ,":" ,c==d)
+// console.log(c,"===",d ,":" ,c===d)
+
+// let marks = 30;
+
+// if (marks > 90) 
+//   console.log("Excellent");
+//  else if (marks > 60) {
+//   console.log("Good");
+//   console.log("work")
+//  }
+//  else 
+//   console.log("Try harder");
+
+// let marks = 25
+
+// let result = (marks > 50) ? "Pass" : "Fail";
+// console.log(result);
+
+// // for loop
+// for (let i = 0; i < 3; i++) {
+//   console.log("i =", i);
+// }
+
+// // for-in: loop over keys
+// let user = { name: "Hemanth", age: 21 };
+// for (let key in user) console.log(key,user[key]);
+
+// // for-of: loop over values (arrays, strings)
+// for (let val of [1, 2, 3]) console.log(val);
+
+// // while loop
+// let i = 0;
+// while (i < 3) {
+//   console.log("while i =", i);
+//   i++;
+// }
+
+// // do-while loop
+// do {
+//   console.log("at least once");
+// } while (false);
